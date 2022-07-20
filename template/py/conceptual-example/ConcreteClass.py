7982d99f57346ff357799aaf9396b72c2d861b77
linth <zhua0404@gmail.com>
2022-07-20 16:10:20 +0800

    add template concept example.

---
+++ template/py/conceptual-example/ConcreteClass.py
@@ -0,0 +1,38 @@
+'''
+Template Method - Concrete class.
+
+Reference:
+    - https://refactoring.guru/design-patterns/template-method/python/example
+'''
+from AbstractClass import AbstractClass
+
+
+class ConcreteClass1(AbstractClass):
+    """
+    Concrete classes have to implement all abstract operations of the base
+    class. They can also override some operations with a default implementation.
+    """
+
+    def required_operations1(self):
+        print("ConcreteClass1 says: Implemented Operation1")
+
+    def required_operations2(self):
+        print("ConcreteClass1 says: Implemented Operation2")
+
+
+class ConcreteClass2(AbstractClass):
+    """
+    Usually, concrete classes override only a fraction of base class'
+    operations.
+    """
+
+    def required_operations1(self) -> None:
+        print("ConcreteClass2 says: Implemented Operation1")
+
+    def required_operations2(self) -> None:
+        print("ConcreteClass2 says: Implemented Operation2")
+
+    def hook1(self) -> None:
+        print("ConcreteClass2 says: Overridden Hook1")
+
+
---
+++ template/py/conceptual-example/main.py
@@ -0,0 +1,29 @@
+'''
+Template Method - main class.
+
+Reference:
+    - https://refactoring.guru/design-patterns/template-method/python/example
+'''
+
+from AbstractClass import AbstractClass
+from ConcreteClass import ConcreteClass1, ConcreteClass2
+
+
+def client_code(abstract_class: AbstractClass):
+    """
+    The client code calls the template method to execute the algorithm. Client
+    code does not have to know the concrete class of an object it works with, as
+    long as it works with objects through the interface of their base class.
+    """
+    abstract_class.template_method()
+    
+    
+    
+if __name__ == '__main__':
+    print("Same client code can work with different subclasses:")
+    client_code(ConcreteClass1())
+    print("")
+
+    print("Same client code can work with different subclasses:")
+    client_code(ConcreteClass2())
+    
