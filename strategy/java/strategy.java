6f7fc662d8c272ab1658eb7b175b4e62d0e0ab92
linth <zhua0404@gmail.com>
2022-10-03 20:15:35 +0800

    add strategy java example.

---
+++ strategy/java/strategy.java
@@ -0,0 +1,12 @@
+package designPattern.Strategy;
+
+/**
+ * Strategy design pattern
+ * 
+ * Reference:
+ *  - https://anexinet.com/blog/strategy-and-factory-patterns-in-a-spring-boot-application-part-i/
+ * 
+ */
+public class Strategy {
+    
+}
