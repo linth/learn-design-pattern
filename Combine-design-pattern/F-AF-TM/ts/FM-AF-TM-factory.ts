bc4f19d615d73ae84df010f5ee25e0f219a8cde1
gerogelin <gerogelin@oringnet.com>
2022-12-01 01:09:51 +0800

    update static factory method and simple factory example.

--- Combine-design-pattern/F-AF-TM/ts/FM-AF-TM-factory.ts
+++ Combine-design-pattern/F-AF-TM/ts/FM-AF-TM-factory.ts
@@ -13,9 +13,12 @@
 
 
 
-// Static Factory Method
+// Static Factory Method, 使用靜態 factory method 來建立 instance.
 {
     abstract class Car {
+        doors: number = 4;
+        windows: number = 4;
+        
         static factory(color: Color): Car {
             switch(color) {
                 case "RED":
@@ -26,6 +29,10 @@
                     return new RedCar();
             }
         }
+
+        abstract show(): void;
+        getCarDoors(): number { return this.doors; }
+        getWindows(): number { return this.windows; }
     }
 
     class Color {
@@ -33,15 +40,24 @@
     }
 
     class RedCar extends Car {
-
+        show(): void {
+            console.log('this is red car.');            
+        }
     }
 
     class BlueCar extends Car {
-
+        show(): void {
+            console.log('this is blue car.');
+        }
     }
 
     let redCar: Car = Car.factory("RED");
     let blueCar: Car = Car.factory("BLUE");
+
+    redCar.show();
+    blueCar.show();
+    console.log(blueCar.getCarDoors()); // car door.
+    console.log(blueCar.getWindows()); // car windows.    
 }
 
 // Simple Factory: 開新的 factory class, 負責生產 Object.
@@ -61,10 +77,16 @@
                     return new NormalCar();
             }
         }
+
+        getCarInfo(): void {
+            console.log('get car information.');            
+        }
     }
     
     class RedCar extends CarFactory {
-    
+        getCarInfo(): void {
+            console.log('get car information.');            
+        }
     }
     
     class BlueCar extends CarFactory {
@@ -81,5 +103,11 @@
 
     let factory: CarFactory = new CarFactory();
     let redCar: Car = factory.createFromColor("RED");
-    
+    let blueCar: Car = factory.createFromColor("BLUE");
+
+    console.log('redCar', redCar);
+    console.log('blueCar', blueCar);
+
+    factory.getCarInfo();
+    // redCar.getCarInfo(); // error.
 }
