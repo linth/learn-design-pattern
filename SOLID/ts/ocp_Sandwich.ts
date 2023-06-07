4dc47c3907d2ea0f73b175e2bd3c2a709740bb7c
gerogelin <gerogelin@oringnet.com>
2023-06-07 21:15:59 +0800

    add empty ocp example.

---
+++ SOLID/ts/ocp_Sandwich.ts
@@ -0,0 +1,24 @@
+/**
+ * Open/Cloas Principle (OCP)
+ * 
+ * Reference:
+ *  - https://medium.com/front-end-weekly/s-o-l-i-d-principles-with-js-examples-db95b44e82e
+ */
+
+class Sizes {
+
+}
+
+
+class SandWich {
+
+}
+
+
+{
+  const run = () => {
+
+  };
+
+  run();
+}
