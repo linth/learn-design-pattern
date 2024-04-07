3e8f42c432b6ee829b03f7bf90b3b140071ea932
linth <zhua0404@gmail.com>
2024-04-07 15:45:40 +0800

    feat: 新增ts singleton-template 範例.

---
+++ singleton/ts/simple-example/Singleton-template.ts
@@ -0,0 +1,32 @@
+/**
+ * Singleton 基本範例
+ */
+
+
+{
+  class Singleton {
+    private static instance: Singleton | null = null;
+
+    constructor() {
+
+    }
+
+    // 獲取 new 出來的 instance.
+    public static getInstance(): Singleton {
+      if (!Singleton.instance) {
+        Singleton.instance = new Singleton();
+      } 
+      return Singleton.instance;
+    }
+
+    public someMethod(): void {
+      console.log('Singleton method called.');
+    }
+  }
+
+  const s1 = new Singleton();
+  const s2 = new Singleton();
+
+  console.log(s1 === s2); // false  
+  console.log(s1.someMethod() === s2.someMethod()); // true
+}
