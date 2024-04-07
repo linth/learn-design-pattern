075b1cbbb0efc62b1ffcd7cc1b98ddd97cbebfc2
linth <zhua0404@gmail.com>
2024-04-07 20:22:24 +0800

    feat: 新增使用generic方式改寫DataFilter

--- singleton/ts/excel-example/excel-data-filter.ts
+++ singleton/ts/excel-example/excel-data-filter.ts
@@ -69,3 +69,90 @@
   const uniqueData = dataFilter.uniqueData([1, 2, 3, 3, 4, 5, 5]);
   console.log("Unique Data:", uniqueData);
 }
+
+
+{
+  class DataFilter<T> {
+    private static instance: DataFilter<any> | null = null;
+
+    private constructor() {
+      
+    }
+
+    static getInstance<T>(): DataFilter<T> {
+      if (!DataFilter.instance) {
+        DataFilter.instance = new DataFilter<T>();
+      }
+      return DataFilter.instance as DataFilter<T>;
+    }
+
+    filterData(data: T[], condition: (item: T) => boolean): T[] {
+      return data.filter(condition);
+    }
+
+    sortData(data: T[], compareFn: (a: T, b: T) => number): T[] {
+      return data.sort(compareFn);
+    }
+
+    paginateData(data: T[], page: number, pageSize: number): T[] {
+      const startIndex = (page - 1) * pageSize;
+      return data.slice(startIndex, startIndex + pageSize);
+    }
+
+    countData(data: T[]): number {
+      return data.length;
+    }
+
+    uniqueData(data: T[]): T[] | number[]{
+      if (typeof data[0] === 'object') {
+        const uniqueMap = new Map<string, T>();
+        for (const item of data) {
+          const key = JSON.stringify(item);
+          uniqueMap.set(key, item);
+        }
+        return Array.from(uniqueMap.values());
+      } else {
+        return Array.from(new Set(data));
+      }
+      
+    }
+  }
+
+  interface Person {
+    id: number;
+    name: string;
+    age: number;
+  }
+
+  const dataFilter = DataFilter.getInstance<Person>();
+  const data: Person[] = [
+    { id: 1, name: "Alice", age: 30 },
+    { id: 2, name: "Bob", age: 25 },
+    { id: 3, name: "Charlie", age: 35 }
+  ]
+
+  const filteredData = dataFilter.filterData(data, item => item.age >= 30);
+  console.log("Filtered Data:", filteredData);
+
+  const sortedData = dataFilter.sortData(data, (a, b) => a.age - b.age);
+  console.log("Sorted Data:", sortedData);
+
+  const paginatedData = dataFilter.paginateData(data, 1, 2);
+  console.log("Paginated Data:", paginatedData);
+
+  const totalCount = dataFilter.countData(data);
+  console.log("Total Count:", totalCount);
+
+  const data2: Person[] = [
+    { id: 1, name: "Alice", age: 30 },
+    { id: 2, name: "Bob", age: 25 },
+    { id: 3, name: "Charlie", age: 35 },
+    { id: 3, name: "Charlie", age: 35 }, // 重複
+    { id: 4, name: "David", age: 40 },
+    { id: 5, name: "Eve", age: 28 },
+    { id: 5, name: "Eve", age: 28 } // 重複
+  ];
+
+  const uniqueData = dataFilter.uniqueData(data2);
+  console.log("Unique Data:", uniqueData);
+}
