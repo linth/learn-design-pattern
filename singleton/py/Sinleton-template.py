00150b7c3e8edef8a2f8569048e06b0ab18c76de
linth <zhua0404@gmail.com>
2024-04-07 15:45:51 +0800

    feat: 新增py singleton-template 範例.

---
+++ singleton/py/Sinleton-template.py
@@ -0,0 +1,26 @@
+
+
+class Singleton:
+    _instance = None
+    
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+
+    def __init__(self):
+        if not hasattr(self, 'initialized'):
+            self.initialized = True
+            
+    def some_method(self):
+        print('Sington method called.')
+        
+
+if __name__ == '__main__':
+    s1 = Singleton()
+    s2 = Singleton()
+    
+    print(s1 is s2) # False
+    print(s1.some_method() is s2.some_method()) # True
+    
+    
