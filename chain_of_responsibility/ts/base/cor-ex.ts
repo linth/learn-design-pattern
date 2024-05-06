60d4b317b9d345952b2590c21320c6c088ded0fb
linth <zhua0404@gmail.com>
2024-05-06 21:56:45 +0800

    feat: add comments.

--- chain_of_responsibility/ts/base/cor-ex.ts
+++ chain_of_responsibility/ts/base/cor-ex.ts
@@ -1,5 +1,8 @@
 /**
- * CoR design pattern.
+ * Chain of Responsibility Pattern (CoR) design pattern. 
  * 
+ * 
+ * Reference:
+ *  - https://juejin.cn/post/7017439694623342623?searchId=20240504002124C2FFA1D188D88289EBBA
  */
 
