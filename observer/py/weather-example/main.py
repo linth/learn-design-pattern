9287e0221fb5699f4e0124bb17e6070685658f89
linth <zhua0404@gmail.com>
2025-09-12 16:20:58 +0800

    feat: add comments.

--- observer/py/weather-example/main.py
+++ observer/py/weather-example/main.py
@@ -5,11 +5,15 @@ from w.forecastDisplay import ForecastDisplay
 
 # 客戶端程式碼
 if __name__ == "__main__":
+    # weather
     weather_station = WeatherStation()
 
+    # build two observer objects.
     current_display = CurrentConditionsDisplay(weather_station)
     forecast_display = ForecastDisplay(weather_station)
 
+    # weather(subject) -setup-> notify (通知主題 subject)
+    # current, forecast(observer), 在裡面放入weather(subject) -> update -> display 
     print("\n第一次測量：")
     weather_station.set_measurements(25, 65, 1020)
 
