5599d74ba2169534bb9d8d180cf87c46282eb6e9
linth <zhua0404@gmail.com>
2025-09-22 00:58:44 +0800

    feat: update comment.

--- singleton/ts/read-world-example/logger-ex.ts
+++ singleton/ts/read-world-example/logger-ex.ts
@@ -6,6 +6,7 @@
 
 {
   enum LogLevel {
+    // 日誌等級
     DEBUG,
     INFO,
     WARNING,
@@ -14,6 +15,13 @@
   }
 
   class Logger {
+    /**
+     * private：表示此屬性為私有的，僅能在 Logger 類別內部存取，外部無法直接讀寫。
+     * static：表示此屬性是類別層級的（class-level），而非實例層級。也就是說，這個變數會被 Logger 類別本身共享，而不是每個 Logger 實例都擁有一份。
+     * _instance：是用來儲存 Logger 類別唯一實例的變數，依照 Singleton（單例）設計模式的慣例，通常用底線 _ 開頭表示私有變數。
+     * Logger | null：代表型別是 Logger 或 null。初始化為 null，表示目前尚未建立 Logger 實例。
+     * = null：賦予初始值為 null，代表一開始沒有實例。
+     */
     private static _instance: Logger | null = null;
 
     private constructor() {}
@@ -51,7 +59,6 @@
       this.log(LogLevel.FATAL, message);
     }
   }
-
 
   const logger = Logger.getInstance();
   logger.debug("This is a debug message");
