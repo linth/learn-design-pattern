8a1517e3d286db642dac6159fa6914e21c905e2e
linth <zhua0404@gmail.com>
2026-02-03 14:22:27 +0800

    feat: add comments.

--- builder/ts/notification_logger/notification.ts
+++ builder/ts/notification_logger/notification.ts
@@ -11,31 +11,37 @@ class Notification {
 export class NotificationBuilder {
   private notification = new Notification();
 
+  // 通知給某位 user
   to(userId: string) {
     this.notification.to = userId;
     return this;
   }
 
+  // 經過那個 channel 通知，如：email, sms, slack
   via(channel: Notification['channel']) {
     this.notification.channel = channel;
     return this;
   }
 
+  // 標題
   title(title: string) {
     this.notification.title = title;
     return this;
   }
 
+  // 內容
   body(body: string) {
     this.notification.body = body;
     return this;
   }
 
+  // 優先權
   priority(level: Notification['priority']) {
     this.notification.priority = level;
     return this;
   }
 
+  // 建立
   build(): Notification {
     if (!this.notification.to || !this.notification.body) {
       throw new Error('Invalid notification');
